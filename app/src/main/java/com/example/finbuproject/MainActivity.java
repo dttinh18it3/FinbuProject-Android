431897fd8d0f05695b1adee28dcba6d58dfe990e
dttinh18it3 <dttinh.18it3@sict.udn.vn>
2020-06-09 22:34:43 +0700

    update project

--- app/src/main/java/com/example/finbuproject/MainActivity.java
+++ app/src/main/java/com/example/finbuproject/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
